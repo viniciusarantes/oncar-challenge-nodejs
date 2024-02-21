8db5414a6a4a7922e93159c26f797bf1d0ec1405
Vinicius Paiva <vinicius.arantespaiva@gmail.com>
2024-02-21 17:29:55 -0300

    Implementando routers com chamada do repository

--- src/routers/VehiclesRouter.ts
+++ src/routers/VehiclesRouter.ts
@@ -1,21 +1,45 @@
 import express from 'express';
+import Vehicle from 'src/models/Vehicle';
+import vehicleRepository from '../repositories/VehiclesRepository';
 
 const vehiclesRouter = express.Router()
 
 vehiclesRouter.post('/', (req, res) => {
-    res.status(201).send()
+    const vehicle: Vehicle = req.body
+    vehicleRepository.create(vehicle, (id) => {
+        if (id) {
+            res.status(201).location(`/vehicles/${id}`).send()
+        } else {
+            res.status(400).send()
+        }
+    })
 })
 
 vehiclesRouter.get('/', (req, res) => {
-    res.json([])
+    vehicleRepository.getAll((vehicles) => res.json(vehicles))
+
 })
 
 vehiclesRouter.get('/:id', (req, res) => {
-    res.json({})
+    const id: number = +req.params.id;
+    vehicleRepository.get(id, (vehicle) => {
+        if (vehicle) {
+            res.json(vehicle)
+        } else {
+            res.status(404).send()
+        }
+    })
 })
 
 vehiclesRouter.delete('/:id', (req, res) => {
-    res.status(200).send()
+    const id = +req.params.id
+    vehicleRepository.delete(id, (notFound) => {
+        if (notFound) {
+            res.status(404).send()
+        } else {
+            res.status(200).send()
+        }
+    })
 })
 
 export default vehiclesRouter;
